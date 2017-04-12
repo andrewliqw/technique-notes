fc9c84e1c2857ccc2dada38eedebc589f774244b
Andrew Li <andrewliqw@outlook.com>
2017-04-12 01:08:53 +0000

    add semicolons

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const path = require('path')
+const path = require('path');
 
 // app.use('/static', express.static(path.join(__dirname, 'src/static')))
-app.use('/static', express.static('src/static'))
+app.use('/static', express.static('src/static'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/index.html'))
-})
+  res.sendFile(path.join(__dirname, 'src/index.html'));
+});
 
 app.get('/docker/docker.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/docker/docker.html'))
-})
+  res.sendFile(path.join(__dirname, 'src/docker/docker.html'));
+});
 
 app.get('/python/pytz.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/python/pytz.html'))
-})
+  res.sendFile(path.join(__dirname, 'src/python/pytz.html'));
+});
 
 app.get('/nodejs/nodejs.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/nodejs/nodejs.html'))
-})
+  res.sendFile(path.join(__dirname, 'src/nodejs/nodejs.html'));
+});
 
 app.listen(8080, '0.0.0.0', () => {
-  console.log('Example app listening on port 8080')
-})
+  console.log('Example app listening on port 8080');
+});
 
 // Hello world using nodejs
 
